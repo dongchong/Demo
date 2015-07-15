e47304ff0d885d6e3cd7da3d7c302df23cfd4568
董崇 <493418130@qq.com>
2015-07-15 17:06:33 +0800

    aaaa

--- app/src/main/java/com/example/dongdong/demo/MyDraw.java
+++ app/src/main/java/com/example/dongdong/demo/MyDraw.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.jar.Attributes;
@@ -14,6 +15,8 @@ import java.util.jar.Attributes;
  */
 public class MyDraw extends View {
     Paint paint = new Paint();
+    public float eventX = 50;
+    public float eventY = 50;
     public MyDraw(Context context) {
         super(context);
     }
@@ -23,7 +26,14 @@ public class MyDraw extends View {
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
         paint.setColor(Color.BLUE);
-        canvas.drawCircle(50,50,10,paint);
+        canvas.drawCircle(eventX,eventY,10,paint);
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        eventX = event.getX();
+        eventY = event.getY();
+        invalidate();
+        return true;
     }
 }
